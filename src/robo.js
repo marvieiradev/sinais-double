bc5895d0de8020c9fd24ca2fe8e74b6c639aa059
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-13 12:49:00 -0300

    feat: change for no-cors method on fetch

--- src/robo.js
+++ src/robo.js
@@ -20,7 +20,7 @@ var len = (el) => {
             url,
             {
                 method: "GET",
-                mode: "cors"
+                mode: "no-cors"
             }
         ).then(
             response => response.json()
